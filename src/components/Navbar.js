75c12c6e03518e8bf36f89199c8b23cc4ddfe869
Rishabh Sinha <sinharishabh402@gmail.com>
2023-04-15 12:20:28 +0530

    Third Commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Stack } from '@mui/material';
 
-import Logo from '../assets/images/Logo.png';
+import Logo from '../assets/images/Logo.jpg';
 
 const Navbar = () => (
   <Stack direction="row" justifyContent="space-around" sx={{ gap: { sm: '123px', xs: '40px' }, mt: { sm: '32px', xs: '20px' }, justifyContent: 'none' }} px="20px">
     <Link to="/">
-      <img src={Logo} alt="logo" style={{ width: '48px', height: '48px', margin: '0px 20px' }} />
+      <img src={Logo} alt="logo" style={{ width: '80px', height: '80px', margin: '0px 20px' }} />
     </Link>
     <Stack
       direction="row"
@@ -16,8 +16,8 @@ const Navbar = () => (
       fontSize="24px"
       alignItems="flex-end"
     >
-      <Link to="/" style={{ textDecoration: 'none', color: '#3A1212', borderBottom: '3px solid #FF2625' }}>Home</Link>
-      <a href="#exercises" style={{ textDecoration: 'none', color: '#3A1212' }}>Exercises</a>
+      <Link to="/" style={{ marginBottom: '30px', textDecoration: 'none', color: '#3A1212', borderBottom: '3px solid #FF2625' }}>Home</Link>
+      <a href="#exercises" style={{ marginBottom: '30px', textDecoration: 'none', color: '#3A1212' }}>Exercises</a>
     </Stack>
   </Stack>
 );
